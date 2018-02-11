b59add3d22bb4d636baa400c98d8f1a3efa8296b
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-02-11 16:56:04 -0500

    Added/Configured rack-cors

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,14 @@ Bundler.require(*Rails.groups)
 
 module AntiSocialNetworkApi
   class Application < Rails::Application
+
+    # Initializes rack-cors
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
