50dd7d24ead331a64f4a1d0ce1e71bced23d3e96
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-02-12 12:27:31 -0500

    Added Index/Show to Post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,28 @@
-class PostsController < ApplicationController
+class API::PostsController < ApplicationController
+
+	def index
+		render json: Post.all
+	end
+
+	def show
+		set_post
+		render json: @post
+	end
+
+
+	private
+
+	def set_post
+		@post = Post.find_by(id: params[:id]);
+	end
 
 	def post_params
-    params.require(:post).permit(
-      [
-        :content,
-        :postType,
-        :photo 
-      ]
-    )
-  end
+	    params.require(:post).permit(
+	      [
+	        :content,
+	        :postType,
+	        :photo 
+	      ]
+	    )
+    end
 end
