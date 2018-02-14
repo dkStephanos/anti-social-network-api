4938ddf5f9c33166ceac45effc705d95290da5ed
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-02-14 11:58:39 -0500

    Set up test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,10 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+# Load sample environment.
+require 'dotenv'
+Dotenv.load('.env.sample')
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
