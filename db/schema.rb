a172f55465e8dd656832dcf4ff5b8df6f772ca93
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-02-15 12:25:30 -0500

    Added avatar_url and bio fields to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180214162809) do
+ActiveRecord::Schema.define(version: 20180215172439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20180214162809) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "avatar_url"
+    t.text "bio"
   end
 
 end
