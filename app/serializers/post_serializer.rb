fa2676aac948f049b7db0b6e92ef47dccd40fa02
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-02-20 14:40:58 -0500

    Added title to Post

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,4 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :content, :postType, :photo
+  attributes :id, :title, :content, :postType, :photo
   belongs_to :user, serializer: PostUserSerializer
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180216175335) do
+ActiveRecord::Schema.define(version: 20180220194027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20180216175335) do
     t.integer "photo_file_size"
     t.datetime "photo_updated_at"
     t.integer "user_id"
+    t.string "title"
   end
 
   create_table "users", force: :cascade do |t|
